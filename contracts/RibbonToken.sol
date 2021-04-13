9b345bee06a4d426aa256d2c56f1bfdc6a900824
John Doe <j@j>
2021-04-13 12:57:19 -0700

    prettify ribbontoken.sol

--- contracts/RibbonToken.sol
+++ contracts/RibbonToken.sol
@@ -6,28 +6,34 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RibbonToken is ERC20Pausable, ERC20Burnable, Ownable{
+contract RibbonToken is ERC20Pausable, ERC20Burnable, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply,
+        address owner
+    ) public ERC20(name, symbol) {
+        // We are minting a total of initialSupply tokens, and it is capped at that value
+        _mint(owner, totalSupply);
+        // Transferring ownership to the new owner
+        transferOwnership(owner);
+    }
 
-	constructor(string memory name, string memory symbol, uint256 totalSupply, address owner)
-		public
-		ERC20(name, symbol) {
-			// We are minting a total of initialSupply tokens, and it is capped at that value
-			_mint(owner, totalSupply);
-			// Transferring ownership to the new owner
-			transferOwnership(owner);
-	}
+    // This function reverts if the caller is not the owner.
+    function pause() external onlyOwner {
+        _pause();
+    }
 
-  // This function reverts if the caller is not the owner.
-  function pause() external onlyOwner {
-    _pause();
-  }
+    // This function reverts if the caller is not the owner.
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 
-  // This function reverts if the caller is not the owner.
-  function unpause() external onlyOwner {
-    _unpause();
-  }
-
-  function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable) {
-    ERC20Pausable._beforeTokenTransfer(from, to, amount);
-  }
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Pausable) {
+        ERC20Pausable._beforeTokenTransfer(from, to, amount);
+    }
 }
