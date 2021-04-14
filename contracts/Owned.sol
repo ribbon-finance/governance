bc05b6d6a87b57dacc4003e6588a0912d8879447
John Doe <j@j>
2021-04-14 14:51:04 -0700

    clean up contracts, make tests configurable with truffle from existing synthetix tests

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -18,7 +18,10 @@ contract Owned {
     }
 
     function acceptOwnership() external {
-        require(msg.sender == nominatedOwner, "You must be nominated before you can accept ownership");
+        require(
+            msg.sender == nominatedOwner,
+            "You must be nominated before you can accept ownership"
+        );
         emit OwnerChanged(owner, nominatedOwner);
         owner = nominatedOwner;
         nominatedOwner = address(0);
@@ -30,7 +33,10 @@ contract Owned {
     }
 
     function _onlyOwner() private view {
-        require(msg.sender == owner, "Only the contract owner may perform this action");
+        require(
+            msg.sender == owner,
+            "Only the contract owner may perform this action"
+        );
     }
 
     event OwnerNominated(address newOwner);
