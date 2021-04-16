03d7cda0cae003f120e857e866cf6ede783ebc58
John Doe <j@j>
2021-04-16 14:51:11 -0700

    add deploy script for staking rewards

---
+++ scripts/deploy-stakingrewards.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+const { TOKEN_PARAMS } = require("../params");
+
+async function main() {
+  // We get the contract to deploy
+  const RibbonStakingRewards = await hre.ethers.getContractFactory("StakingRewards");
+  const ribbonStakingRewards = await RibbonStakingRewards.deploy(
+    STAKING_REWARDS_PARAMS.OWNER,
+    STAKING_REWARDS_PARAMS.REWARDS_DIST_ADDR,
+    STAKING_REWARDS_PARAMS.REWARDS_TOKEN,
+    STAKING_REWARDS_PARAMS.STAKING_TOKEN
+  );
+
+  await ribbonStakingRewards.deployed();
+
+  console.log(
+    `\nRibbon Staking Rewards is deployed at ${ribbonStakingRewards.address}, verify with https://etherscan.io/proxyContractChecker?a=${ribbonStakingRewards.address}\n`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
