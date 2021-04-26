d7ee83b2b6823b78b3ff96f0c01751fd1b5e6f11
John Doe <j@j>
2021-04-26 09:55:32 -0700

    remove event

--- contracts/RibbonStakingRewards.sol
+++ contracts/RibbonStakingRewards.sol
@@ -125,7 +125,8 @@ contract StakingRewards is
     }
 
     function weeksStaked(address account) public view returns (uint256) {
-        return _numWeeksPassed(block.timestamp).sub(userStakeStart[account]);
+        return
+            _numWeeksPassed(block.timestamp).sub(userStakeStart[account]);
     }
 
     function getRewardForDuration() external view override returns (uint256) {
@@ -159,8 +160,8 @@ contract StakingRewards is
         _totalSupply = _totalSupply.sub(amount);
         _balances[msg.sender] = _balances[msg.sender].sub(amount);
         stakingToken.safeTransfer(msg.sender, amount);
-        if (_balances[msg.sender] == 0) {
-            userStakeStart[msg.sender] = _numWeeksPassed(block.timestamp);
+        if(_balances[msg.sender] == 0) {
+          userStakeStart[msg.sender] = _numWeeksPassed(block.timestamp);
         }
         emit Withdrawn(msg.sender, amount);
     }
@@ -268,5 +269,4 @@ contract StakingRewards is
     event RewardPaid(address indexed user, uint256 reward);
     event RewardsDurationUpdated(uint256 newDuration);
     event Recovered(address token, uint256 amount);
-    event startEmissionUpdated(uint256 startEmission);
 }
