c71f6345e27543047549f1704f29368f3f9943c4
Chan Jing Hong <jinghongchan@gmail.com>
2021-06-01 14:52:05 +0800

    Update variable parameter for liquidity mining contract depending on network

--- scripts/deploy-rETH-THETA-stakingrewards.js
+++ scripts/deploy-rETH-THETA-stakingrewards.js
@@ -1,17 +1,54 @@
 const hre = require("hardhat");
 const { STAKING_REWARDS_rETHTHETA_PARAMS } = require("../params");
+const moment = require("moment");
 
 async function main() {
-  // We get the contract to deploy
+  const [deployer] = await ethers.getSigners();
+  const network = hre.network.name;
+
+  /** We get the contract to deploy */
   const RibbonStakingRewards = await hre.ethers.getContractFactory(
     "StakingRewards"
   );
+
+  /** Set parameter */
+  const owner =
+    network === "kovan"
+      ? deployer.address
+      : STAKING_REWARDS_rETHTHETA_PARAMS.OWNER;
+  const rewardsDistAddress =
+    network === "kovan"
+      ? deployer.address
+      : STAKING_REWARDS_rETHTHETA_PARAMS.REWARDS_DIST_ADDR;
+  /** Kovan will be $KRBN */
+  const rewardsToken =
+    network === "kovan"
+      ? "0x567e482AF973187648Af9FC56d2Caec212c1CAca"
+      : STAKING_REWARDS_rETHTHETA_PARAMS.REWARDS_TOKEN;
+  const stakingToken =
+    network === "kovan"
+      ? "0x5B8E6eaB6502CC642d00A55F0d8B5f5557c94Bc5"
+      : STAKING_REWARDS_rETHTHETA_PARAMS.STAKING_TOKEN;
+  /**
+   * Kovan will always be set to next saturday 1200 UTC
+   */
+  const startEmission =
+    network === "kovan"
+      ? moment()
+          .startOf("isoWeek")
+          .add(1, "week")
+          .utc(true)
+          .day("saturday")
+          .unix()
+      : STAKING_REWARDS_rETHTHETA_PARAMS.START_EMISSION;
+
+  /** Deploy Address */
   const ribbonStakingRewards = await RibbonStakingRewards.deploy(
-    STAKING_REWARDS_rETHTHETA_PARAMS.OWNER,
-    STAKING_REWARDS_rETHTHETA_PARAMS.REWARDS_DIST_ADDR,
-    STAKING_REWARDS_rETHTHETA_PARAMS.REWARDS_TOKEN,
-    STAKING_REWARDS_rETHTHETA_PARAMS.STAKING_TOKEN,
-    STAKING_REWARDS_rETHTHETA_PARAMS.START_EMISSION
+    owner,
+    rewardsDistAddress,
+    rewardsToken,
+    stakingToken,
+    startEmission
   );
 
   await ribbonStakingRewards.deployed();
@@ -19,6 +56,17 @@ async function main() {
   console.log(
     `\nRibbon rETH-THETA Staking Rewards is deployed at ${ribbonStakingRewards.address}, verify with https://etherscan.io/proxyContractChecker?a=${ribbonStakingRewards.address}\n`
   );
+
+  await hre.run("verify:verify", {
+    address: ribbonStakingRewards.address,
+    constructorArguments: [
+      owner,
+      rewardsDistAddress,
+      rewardsToken,
+      stakingToken,
+      startEmission,
+    ],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
