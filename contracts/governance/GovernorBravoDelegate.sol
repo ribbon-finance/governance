251e4ad7a2885b4942268d65705abf711a328935
kenchangh <guanhao3797@gmail.com>
2021-10-27 16:43:54 +0800

    Working permit

--- contracts/governance/GovernorBravoDelegate.sol
+++ contracts/governance/GovernorBravoDelegate.sol
@@ -27,7 +27,7 @@ contract GovernorBravoDelegate is GovernorBravoDelegateStorageV1, GovernorBravoE
     uint public constant MAX_VOTING_DELAY = 40320; // About 1 week
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    uint public constant quorumVotes = 400000e18; // 400,000 = 4% of sRBN
+    uint public constant quorumVotes = 1000000e18; // 1,000,000 = 1% of sRBN
 
     /// @notice The maximum number of actions that can be included in a proposal
     uint public constant proposalMaxOperations = 10; // 10 actions
