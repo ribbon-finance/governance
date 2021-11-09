5e91157579358c8602f87325d09c717f4a9cd4cc
chudnov <jacob4seinfeld@gmail.com>
2021-11-09 12:01:20 -0800

    vesting -> voting

--- contracts/interfaces/IVotingEscrow.sol
+++ contracts/interfaces/IVotingEscrow.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-interface IVestingEscrow {
+interface IVotingEscrow {
   function totalLocked() external view returns (uint256);
 
   function redeemRBN(uint256 amountToRedeem) external;
