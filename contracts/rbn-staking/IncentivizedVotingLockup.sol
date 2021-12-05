23c09486c24284f265ad16a7bd3c5ba81d79a537
Your Name <you@example.com>
2021-12-04 19:44:30 -0800

    add events

--- contracts/rbn-staking/IncentivizedVotingLockup.sol
+++ contracts/rbn-staking/IncentivizedVotingLockup.sol
@@ -54,6 +54,33 @@ contract IncentivisedVotingLockup is
   );
   event Withdraw(address indexed provider, uint256 value, uint256 ts);
   event ContractStopped(bool contractStopped);
+  /// @notice An event thats emitted when an account changes its delegate
+  event DelegateSet(
+    address indexed delegator,
+    address indexed toDelegate,
+    uint256 amount,
+    uint256 cancelTime,
+    uint256 expireTime
+  );
+  /// @notice An event thats emitted when an account changes its delegate
+  event DelegateExtended(
+    address indexed delegator,
+    address indexed toDelegate,
+    uint256 amount,
+    uint256 cancelTime,
+    uint256 expireTime
+  );
+  /// @notice An event thats emitted when an account removes its delegate
+  event DelegateRemoved(
+    address indexed delegator,
+    address indexed delegateeToRemove
+  );
+  /// @notice An event thats emitted when a delegate account's vote balance changes
+  event DelegateVotesChanged(
+    address indexed delegate,
+    uint256 previousBalance,
+    uint256 newBalance
+  );
 
   // RBN Redeemer contract
   address public rbnRedeemer;
@@ -74,11 +101,36 @@ contract IncentivisedVotingLockup is
   uint256 public totalShares;
   uint256 public totalLocked;
   Point[] public pointHistory;
+  bool public contractStopped;
   mapping(address => Point[]) public userPointHistory;
   mapping(address => uint256) public userPointEpoch;
   mapping(uint256 => int128) public slopeChanges;
   mapping(address => LockedBalance) public locked;
-  bool public contractStopped;
+  /// @notice A record of votes checkpoints for each account, by index
+  mapping(address => mapping(uint32 => DelegateCheckpoint))
+    public delegateCheckpoints;
+
+  /// @notice The number of checkpoints for each account
+  mapping(address => uint32) public numCheckpoints;
+
+  /// @notice The EIP-712 typehash for the contract's domain
+  bytes32 public constant DOMAIN_TYPEHASH =
+    keccak256(
+      "EIP712Domain(string name,uint256 chainId,address verifyingContract)"
+    );
+
+  /// @notice The EIP-712 typehash for the delegation struct used by the contract
+  bytes32 public constant DELEGATION_TYPEHASH =
+    keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
+
+  /// @notice The EIP-712 typehash for the permit struct used by the contract
+  bytes32 public constant PERMIT_TYPEHASH =
+    keccak256(
+      "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+    );
+
+  /// @notice A record of states for signing / validating signatures
+  mapping(address => uint256) public nonces;
 
   // Voting token - Checkpointed view only ERC20
   /// @notice EIP-20 token name for this token
@@ -100,6 +152,12 @@ contract IncentivisedVotingLockup is
     int112 amount;
     int112 shares;
     uint32 end;
+  }
+
+  /// @notice A checkpoint for marking number of votes from a given block
+  struct DelegateCheckpoint {
+    uint32 fromBlock;
+    uint96 votes;
   }
 
   enum LockAction {CREATE_LOCK, INCREASE_LOCK_AMOUNT, INCREASE_LOCK_TIME}
@@ -747,7 +805,6 @@ contract IncentivisedVotingLockup is
       upoint.bias -
       (upoint.slope * SafeCast.toInt128(int256(blockTime - upoint.ts)));
     if (upoint.bias >= 0) {
-      // We do < 0 check earlier
       return StableMath.toUint96(SafeCast.toUint256(upoint.bias));
     } else {
       return 0;
@@ -850,5 +907,33 @@ contract IncentivisedVotingLockup is
       chainId := chainid()
     }
     return chainId;
+  }
+
+  function safe32(uint256 n, string memory errorMessage)
+    internal
+    pure
+    returns (uint32)
+  {
+    require(n < 2**32, errorMessage);
+    return uint32(n);
+  }
+
+  function add96(
+    uint96 a,
+    uint96 b,
+    string memory errorMessage
+  ) internal pure returns (uint96) {
+    uint96 c = a + b;
+    require(c >= a, errorMessage);
+    return c;
+  }
+
+  function sub96(
+    uint96 a,
+    uint96 b,
+    string memory errorMessage
+  ) internal pure returns (uint96) {
+    require(b <= a, errorMessage);
+    return a - b;
   }
 }
