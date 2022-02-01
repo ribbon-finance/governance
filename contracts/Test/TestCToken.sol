fa86a8107b97137548859c1c953449517a95a0a0
Your Name <you@example.com>
2022-02-01 01:07:38 -0800

    fix test

--- contracts/Test/TestCToken.sol
+++ contracts/Test/TestCToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 contract CToken{
     address public underlying;
+    bool public isCEther;
 
     function setUnderlying(address _underlying) public {
         underlying = _underlying;
