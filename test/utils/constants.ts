22c6e179b343ca1b117ff31596eb5eabc2e95a43
Your Name <you@example.com>
2022-03-29 23:21:19 -0700

    add more unit tests for fee custody

--- test/utils/constants.ts
+++ test/utils/constants.ts
@@ -1,6 +1,8 @@
 /* eslint-disable max-classes-per-file */
 import { utils, BigNumber as BN } from "ethers"
 
+require("dotenv").config();
+
 /**
  * @notice This file contains constants relevant across the mStable test suite
  * Wherever possible, it should conform to fixed on chain vars
@@ -28,3 +30,53 @@ export const FIVE_DAYS = BN.from(60 * 60 * 24 * 5)
 export const TEN_DAYS = BN.from(60 * 60 * 24 * 10)
 export const ONE_WEEK = BN.from(60 * 60 * 24 * 7)
 export const ONE_YEAR = BN.from(60 * 60 * 24 * 365)
+
+export const TEST_URI = process.env.TEST_URI
+
+/**
+ * Assets
+ */
+export const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+export const WBTC_ADDRESS = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"
+export const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+export const AAVE_ADDRESS = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9"
+
+export const WETH_OWNER_ADDRESS = "0x2feb1512183545f48f6b9c5b4ebfcaf49cfca6f3"
+export const WBTC_OWNER_ADDRESS = "0x7abe0ce388281d2acf297cb089caef3819b13448"
+export const USDC_OWNER_ADDRESS = "0xe11970f2f3de9d637fb786f2d869f8fea44195ac"
+export const AAVE_OWNER_ADDRESS = "0xf25e8b750b0b9d5468ee689fecdb6c089a6bb923"
+
+/**
+ * Chainlink Oracles
+ *
+ * https://data.chain.link/
+ * https://docs.chain.link/docs/avalanche-price-feeds
+ */
+
+ export const ETH_PRICE_ORACLE = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
+ export const BTC_PRICE_ORACLE = "0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c"
+ export const USDC_PRICE_ORACLE = "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6"
+ export const AAVE_PRICE_ORACLE = "0x547a514d5e3769680ce22b2361c10ea13619e8a9"
+ export const BAD_PRICE_ORACLE = "0x6df09e975c830ecae5bd4ed9d90f3a95a4f88012"
+
+/**
+ * DEX Routers and Factories
+ */
+
+ export const DEX_ROUTER = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+ export const DEX_FACTORY = "0x1F98431c8aD98523631AE4a59f267346ea31F984"
+
+/**
+ * Swap Pools
+ */
+
+export const ETH_USDC_POOL = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
+export const ETH_BTC_POOL = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
+export const ETH_BTC_POOL_S = "0x4585fe77225b41b697c938b018e2ac67ac5a20c0"
+export const BTC_USDC_POOL = "0x99ac8ca7087fa4a2a1fb6357269965a2014abc35"
+export const ETH_AAVE_POOL = "0x5ab53ee1d50eef2c1dd3d5402789cd27bb52c1bb"
+
+export const POOL_LARGE_FEE = 3000
+export const POOL_SMALL_FEE = 500
+
+export const PCT_AllOC_FOR_LOCKERS = 5000 // 50%
