056f07f1436f2381dc1c29d41cd9dee1c4cc98dd
kenchangh <guanhao3797@gmail.com>
2022-04-05 10:15:04 +0800

    test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,7 +73,7 @@ module.exports = {
     timeout: 200000,
   },
   vyper: {
-    compilers: [{ version: "0.2.7" }, { version: "0.2.15" }, { version: "0.3.1" }],
+    version: "0.2.7",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
